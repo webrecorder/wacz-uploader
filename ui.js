a44a39a7540f6e69a0fa3efdb511c747168229f8
sua yoo <sua@suayoo.com>
2022-10-26 11:08:16 -0700

    fix drag drop

--- ui.js
+++ ui.js
@@ -1,7 +1,11 @@
 /**
  * App UI state
  */
-import { createMachine, interpret, assign } from 'https://unpkg.com/xstate@4.33.6/dist/xstate.web.js'
+import {
+  createMachine,
+  interpret,
+  assign,
+} from 'https://unpkg.com/xstate@4.33.6/dist/xstate.web.js'
 
 // Context stored in app state
 const initialContext = {
@@ -32,16 +36,22 @@ export class App {
   states = {
     initial: {
       entry: () => this.renderInitial(),
+      states: {
+        noValidFiles: {
+          entry: () => this.renderNoValidFiles(),
+        },
+      },
       on: {
+        NO_VALID_FILES_SELECTED: '.noValidFiles',
         UPLOAD_FILE_LIST_START: {
           target: 'uploadingFileList',
-          actions: assign({ fileMap: (ctx, { fileList }) => new Map(
-            fileList.map(file => ([
-              file.name,
-              { name: file.name, file }
-            ]))
-          )}),
-        }
+          actions: assign({
+            fileMap: (ctx, { fileList }) =>
+              new Map(
+                fileList.map((file) => [file.name, { name: file.name, file }])
+              ),
+          }),
+        },
       },
     },
     uploadingFileList: {
@@ -74,7 +84,7 @@ export class App {
               },
             }),
             (ctx, evt) => this.renderDeletedFile(evt),
-          ]
+          ],
         },
         UPDATE_FILE_NAME: {
           actions: [
@@ -89,7 +99,7 @@ export class App {
               const data = fileMap.get(file.name)
               this.renderFileNames(data)
             },
-          ]
+          ],
         },
         UPDATE_FILE_DESCRIPTION: {
           actions: assign({
@@ -101,7 +111,7 @@ export class App {
           }),
         },
         FILE_UPLOAD_START: {
-          actions: (ctx, evt) => this.renderFileStart(evt)
+          actions: (ctx, evt) => this.renderFileStart(evt),
         },
         FILE_UPLOAD_ERROR: {
           actions: [
@@ -112,8 +122,8 @@ export class App {
                 return ctx.fileMap
               },
             }),
-            (ctx, evt) => this.renderFileError(evt)
-          ]
+            (ctx, evt) => this.renderFileError(evt),
+          ],
         },
         FILE_UPLOAD_SUCCESS: {
           actions: [
@@ -129,31 +139,36 @@ export class App {
               this.renderFileSuccess(data)
               this.renderFileDetail(data)
             },
-          ]
+          ],
         },
         UPLOAD_FILE_LIST_ERROR: {
           actions: (ctx, evt) => this.renderErrorMessage(evt),
         },
-        UPLOAD_FILE_LIST_FINISH: [{
-          target: '.uploadIncomplete',
-          cond: (ctx, evt) => evt.rejected && evt.rejected.size > 0,
-        }, {
-          target: '.uploadComplete',
-          cond: (ctx, evt) => evt.completed && (!evt.rejected || evt.rejected.size === 0),
-          actions: assign({
-            fileMap: (ctx, { completed }) => {
-              const map = new Map(completed)
-              map.forEach((value, key) => {
-                const ctxValue = ctx.fileMap.get(key)
-                map.set(key, {
-                  ...ctxValue,
-                  ...value,
+        UPLOAD_FILE_LIST_FINISH: [
+          {
+            target: '.uploadIncomplete',
+            cond: (ctx, evt) => evt.rejected && evt.rejected.size > 0,
+            actions: () => this.renderDoneLoadingDetails()
+          },
+          {
+            target: '.uploadComplete',
+            cond: (ctx, evt) =>
+              evt.completed && (!evt.rejected || evt.rejected.size === 0),
+            actions: assign({
+              fileMap: (ctx, { completed }) => {
+                const map = new Map(completed)
+                map.forEach((value, key) => {
+                  const ctxValue = ctx.fileMap.get(key)
+                  map.set(key, {
+                    ...ctxValue,
+                    ...value,
+                  })
                 })
-              })
-              return map
-            }
-          }),
-        }],
+                return map
+              },
+            }),
+          },
+        ],
       },
     },
     incompleteFileListUpload: {
@@ -177,15 +192,15 @@ export class App {
           entry: (ctx, evt) => this.renderCreatingSiteError(evt),
           on: {
             BACK: '#uploadingFileList.uploadComplete',
-          }
-        }
+          },
+        },
       },
       on: {
         UPLOAD_SITE_START: {
           actions: () => console.debug('TODO handle UPLOAD_SITE_START'),
         },
         UPLOAD_SITE_ERROR: '.error',
-        UPLOAD_SITE_FINISH: 'done'
+        UPLOAD_SITE_FINISH: 'done',
       },
     },
     done: {
@@ -202,31 +217,89 @@ export class App {
 
   constructor({ wrapper }) {
     this.appRoot = document.getElementById('appRoot')
-    this.stateService = interpret(createMachine({
-      initial: 'initial',
-      predictableActionArguments: true,
-      context: initialContext,
-      states: this.states,
-    }))
+    this.stateService = interpret(
+      createMachine({
+        initial: 'initial',
+        predictableActionArguments: true,
+        context: initialContext,
+        states: this.states,
+      })
+    )
       .onTransition((state) => console.debug(state.value))
       .start()
     this.wrapperService = {
       uploadWrappedArchives: (...args) => wrapper.uploadWrappedArchives(...args),
+      uploadFromDropEvent: (...args) => wrapper.uploadFromDropEvent(...args),
+      uploadFromFileInputEvent: (...args) => wrapper.uploadFromFileInputEvent(...args),
     }
 
-    wrapper.addEventListener('uploadfilestart', (evt) => this.stateService.send('FILE_UPLOAD_START', evt))
-    wrapper.addEventListener('uploadfilefinish', (evt) => this.stateService.send('FILE_UPLOAD_SUCCESS', evt))
-    wrapper.addEventListener('uploadfileerror', (evt) => this.stateService.send('FILE_UPLOAD_ERROR', evt))
-    wrapper.addEventListener('uploadfileliststart', (evt) => this.stateService.send('UPLOAD_FILE_LIST_START', evt))
-    wrapper.addEventListener('uploadfilelistfinish', (evt) => this.stateService.send('UPLOAD_FILE_LIST_FINISH', evt))
-    wrapper.addEventListener('uploadfilelisterror', (evt) => this.stateService.send('UPLOAD_FILE_LIST_ERROR', evt))
-    wrapper.addEventListener('uploadsitestart', (evt) => this.stateService.send('UPLOAD_SITE_START', evt))
-    wrapper.addEventListener('uploadsitefinish', (evt) => this.stateService.send('UPLOAD_SITE_FINISH', evt))
-    wrapper.addEventListener('uploadsiteerror', (evt) => this.stateService.send('UPLOAD_SITE_ERROR', evt))
+    
+    wrapper.addEventListener('novalidfiles', (evt) =>
+      this.stateService.send('NO_VALID_FILES_SELECTED', evt)
+    )
+    wrapper.addEventListener('uploadfilestart', (evt) =>
+      this.stateService.send('FILE_UPLOAD_START', evt)
+    )
+    wrapper.addEventListener('uploadfilefinish', (evt) =>
+      this.stateService.send('FILE_UPLOAD_SUCCESS', evt)
+    )
+    wrapper.addEventListener('uploadfileerror', (evt) =>
+      this.stateService.send('FILE_UPLOAD_ERROR', evt)
+    )
+    wrapper.addEventListener('uploadfileliststart', (evt) =>
+      this.stateService.send('UPLOAD_FILE_LIST_START', evt)
+    )
+    wrapper.addEventListener('uploadfilelistfinish', (evt) =>
+      this.stateService.send('UPLOAD_FILE_LIST_FINISH', evt)
+    )
+    wrapper.addEventListener('uploadfilelisterror', (evt) =>
+      this.stateService.send('UPLOAD_FILE_LIST_ERROR', evt)
+    )
+    wrapper.addEventListener('uploadsitestart', (evt) =>
+      this.stateService.send('UPLOAD_SITE_START', evt)
+    )
+    wrapper.addEventListener('uploadsitefinish', (evt) =>
+      this.stateService.send('UPLOAD_SITE_FINISH', evt)
+    )
+    wrapper.addEventListener('uploadsiteerror', (evt) =>
+      this.stateService.send('UPLOAD_SITE_ERROR', evt)
+    )
   }
 
   renderInitial() {
-    console.debug('TODO renderInitial')
+    window.dropzone.addEventListener('drag', (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+    })
+    window.dropzone.addEventListener('dragstart', (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+    })
+    window.dropzone.addEventListener('dragend', (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+    })
+    window.dropzone.addEventListener('dragleave', (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+    })
+    window.dropzone.addEventListener('dragover', (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+    })
+    window.dropzone.addEventListener('dragenter', (e) => {
+      if (!window.dropzone.contains(e.relatedTarget)) {
+        window.dropzone.querySelector('sl-animation').setAttribute('play', true)
+      }
+    })
+    window.dropzone.addEventListener('drop', (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+      this.wrapperService.uploadFromDropEvent(e)
+    })
+    window.fileInput.addEventListener('change', (e) => {
+      this.wrapperService.uploadFromFileInputEvent(e)
+    })
   }
 
   renderUploading({ fileMap }) {
@@ -238,19 +311,21 @@ export class App {
     fileMap.forEach((value) => {
       this.renderFile(value)
 
-      if (value.rejected) {
-        this.renderFileError(value)
-      } else {
-        this.renderFileSuccess(value)
-        // this.renderFileDetail(value)
-      }
+      // if (value.rejected) {
+      //   this.renderFileError(value)
+      // } else {
+      //   this.renderFileSuccess(value)
+      //   this.renderFileDetail(value)
+      // }
     })
   }
 
   renderFile({ name, size, file }) {
     const template = document.querySelector('#fileListItem')
     const listItem = template.content.cloneNode(true)
-    listItem.querySelector('[data-file-name]').setAttribute('data-file-name', file.name)
+    listItem
+      .querySelector('[data-file-name]')
+      .setAttribute('data-file-name', file.name)
     listItem.querySelector('.name').innerText = name || file.name
     listItem.querySelector('.size').setAttribute('value', size || file.size)
     listItem.querySelector('.delete-btn').addEventListener('click', (e) => {
@@ -259,16 +334,22 @@ export class App {
     })
     this.appRoot.querySelector('.file-list').appendChild(listItem)
   }
-
-  renderFileDetail({ name, file }) {
+  
+  renderDoneLoadingDetails() {
     const loadingIndicator = this.appRoot.querySelector('.file-details-loading')
     if (loadingIndicator) {
       loadingIndicator.parentNode.removeChild(loadingIndicator)
     }
+  }
+
+  renderFileDetail({ name, file }) {
+    this.renderDoneLoadingDetails()
 
     const template = document.querySelector('#fileDetailItem')
     const detail = template.content.cloneNode(true)
-    detail.querySelector('[data-file-name]').setAttribute('data-file-name', file.name)
+    detail
+      .querySelector('[data-file-name]')
+      .setAttribute('data-file-name', file.name)
     detail.querySelector('.name').innerText = name || file.name
     detail.querySelector('.name-input').setAttribute('value', name || file.name)
     detail.querySelector('.edit-btn').addEventListener('click', (e) => {
@@ -280,7 +361,8 @@ export class App {
       form.addEventListener('submit', (e) => {
         e.preventDefault()
         this.stateService.send('UPDATE_FILE_NAME', {
-          file, name: form.querySelector('input').value,
+          file,
+          name: form.querySelector('input').value,
         })
         header.classList.remove('hidden')
         form.classList.add('hidden')
@@ -288,28 +370,40 @@ export class App {
     })
     detail.querySelector('textarea').addEventListener('change', (e) => {
       this.stateService.send('UPDATE_FILE_DESCRIPTION', {
-        file, description: e.target.value,
+        file,
+        description: e.target.value,
       })
     })
     this.appRoot.querySelector('.file-detail-list').appendChild(detail)
   }
 
   renderFileStart({ file }) {
-    const listItem = this.appRoot.querySelector(`.file-list [data-file-name="${file.name}"]`)
-    listItem.querySelector('.status').innerHTML = '<sl-spinner class="icon"></sl-spinner>'    
+    const listItem = this.appRoot.querySelector(
+      `.file-list [data-file-name="${file.name}"]`
+    )
+    listItem.querySelector('.status').innerHTML =
+      '<sl-spinner class="icon"></sl-spinner>'
   }
 
   renderFileError({ file }) {
-    const listItem = this.appRoot.querySelector(`.file-list [data-file-name="${file.name}"]`)
+    const listItem = this.appRoot.querySelector(
+      `.file-list [data-file-name="${file.name}"]`
+    )
     listItem.querySelector('.file-info').setAttribute('aria-disabled', true)
-    listItem.querySelector('.status').innerHTML = '<sl-icon class="icon danger" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.83/dist/assets/icons/exclamation-circle-fill.svg"></sl-icon>'
+    listItem.querySelector('.status').innerHTML =
+      '<sl-icon class="icon danger" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.83/dist/assets/icons/exclamation-circle-fill.svg"></sl-icon>'
   }
 
   renderFileSuccess({ file }) {
-    const listItem = this.appRoot.querySelector(`.file-list [data-file-name="${file.name}"]`)
-    listItem.querySelector('.status').innerHTML = '<sl-icon class="icon success" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.83/dist/assets/icons/check-circle-fill.svg"></sl-icon>'
+    const listItem = this.appRoot.querySelector(
+      `.file-list [data-file-name="${file.name}"]`
+    )
+    listItem.querySelector('.status').innerHTML =
+      '<sl-icon class="icon success" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.83/dist/assets/icons/check-circle-fill.svg"></sl-icon>'
     listItem.querySelector('.file-info').addEventListener('click', (e) => {
-      const item = this.appRoot.querySelector(`.file-detail-list [data-file-name="${file.name}"]`)
+      const item = this.appRoot.querySelector(
+        `.file-detail-list [data-file-name="${file.name}"]`
+      )
       item.scrollIntoView({ behavior: 'smooth' })
       item.classList.add('selected')
       window.setTimeout(() => {
@@ -319,15 +413,19 @@ export class App {
   }
 
   renderDeletedFile({ file }) {
-    const items = this.appRoot.querySelectorAll(`[data-file-name="${file.name}"]`)
-    Array.from(items).forEach(item => {
+    const items = this.appRoot.querySelectorAll(
+      `[data-file-name="${file.name}"]`
+    )
+    Array.from(items).forEach((item) => {
       item.classList.add('hidden')
     })
   }
 
   renderFileNames({ name, file }) {
-    const items = this.appRoot.querySelectorAll(`[data-file-name="${file.name}"] .name`)
-    Array.from(items).forEach(item => {
+    const items = this.appRoot.querySelectorAll(
+      `[data-file-name="${file.name}"] .name`
+    )
+    Array.from(items).forEach((item) => {
       item.innerText = name
     })
   }
@@ -373,6 +471,12 @@ export class App {
       // this.stateService.send('RESTART')
     })
     this.appRoot.replaceChildren(section)
+  }
+
+  renderNoValidFiles() {
+    const dropzone = this.appRoot.querySelector('#dropzone')
+    dropzone.classList.add('has-error')
+    dropzone.querySelector('.error-message').classList.remove('hidden')
   }
 
   renderErrorMessage({ error }) {
